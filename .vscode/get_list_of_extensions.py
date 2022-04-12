858507fe2317eec2508d7980c0966f898a0545d1
Rasul Kireev <rasul.kireev@guycarp.com>
2022-04-12 00:09:00 -0400

    added vscode config

---
+++ .vscode/get_list_of_extensions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from pathlib import Path
+
+home_path = Path.home()
+current_dir = Path.cwd()
+
+vscode_extensions_dir = home_path / '.vscode' / 'extensions'
+vscode_extensions = list(vscode_extensions_dir.glob('*'))
+
+extensions_file = current_dir / 'extensions'
+
+with open(extensions_file, mode="w") as outfile:
+    for extension in vscode_extensions:
+        outfile.write("%s\n" % extension.name)
+
+# Cron job to run this every Sunday at 00:00
+#  0 0 * * 0 cd /Users/u1082926/ps/code/dotfiles/.vscode && /usr/local/bin/python3 get_list_of_extensions.py >> ~/ps/code/dotfiles/.vscode/cron.txt 2>&1
